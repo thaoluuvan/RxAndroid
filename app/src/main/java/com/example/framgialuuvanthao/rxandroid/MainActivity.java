7c28c27ebe2d4a186ae4b98b9710c489ce5f31dd
thaoluuvan <luu.van.thao@framgia.com>
2018-11-02 16:35:00 +0700

    using Observable.fromCallable

--- app/src/main/java/com/example/framgialuuvanthao/rxandroid/MainActivity.java
+++ app/src/main/java/com/example/framgialuuvanthao/rxandroid/MainActivity.java
@@ -4,43 +4,60 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 import rx.Observable;
 import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-    public Observable<List<String>> listObservable = Observable.just(getColorList());
+
     public static String TAG = MainActivity.class.getSimpleName();
     RecyclerView recyclerViewColor;
     SimpleStringAdapter simpleStringAdapter;
-
+    Observable<List<String>> tvShowObservable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
-        listObservable.subscribe(new Observer<List<String>>() {
 
+        tvShowObservable = Observable.fromCallable(new Callable<List<String>>() {
             @Override
-            public void onCompleted() {
-            }
-
-            @Override
-            public void onError(Throwable e) {
-            }
-
-            @Override
-            public void onNext(List<String> colors) {
-                simpleStringAdapter.setStrings(colors);
+            public List<String> call() throws Exception {
+                Log.d(TAG, "onNext: " + Thread.currentThread().getName());
+                return getColorList();
             }
         });
-    }
 
+        tvShowObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<List<String>>() {
+                    @Override
+                    public void onCompleted() {
+                        Toast.makeText(MainActivity.this, "Loaded data", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                    }
+
+                    @Override
+                    public void onNext(List<String> tvShows) {
+                        Log.d(TAG, "onNext: " + Thread.currentThread().getName());
+                        simpleStringAdapter.setStrings(tvShows);
+                    }
+                });
+    }
 
     private void initViews() {
         simpleStringAdapter = new SimpleStringAdapter(this);
@@ -51,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static List<String> getColorList() {
         ArrayList<String> colors = new ArrayList<>();
+        colors.add("thao handsome");
         colors.add("blue");
         colors.add("green");
         colors.add("red");
